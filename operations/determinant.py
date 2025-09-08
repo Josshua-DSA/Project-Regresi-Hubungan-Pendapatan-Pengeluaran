7780bf0956dba16aff5834260d52c82cba6d1ad7
Josh <alexdenwall@gmail.com>
2025-09-08 23:34:44 +0700

    Revert "pindahkan fungsi find_determinant ke operations/determinant.py"
    This reverts commit 57bb7a4903352ece7ced962e32d543d0b91822f1.

--- operations/determinant.py
+++
@@ -1,7 +0,0 @@
-Python
-#matriks/operations/determinant
-
-def find_determinant(matrix):
-    if matrix.rows != 2 or matrix.cols != 2:
-        raise ValueError("Saat ini hanya mendukung matriks 2x2.")
-    return matrix.data[0][0] * matrix.data[1][1] - matrix.data[0][1] * matrix.data[1][0]
